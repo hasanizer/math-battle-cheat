2c85b9d48c3539e6081ca3cf788ea5ab3aeb4b50
hasanul hakim <awesome.hasanul@gmail.com>
2019-07-15 15:15:59 +0700

    Fix operator

--- answer.js
+++ answer.js
@@ -1,33 +1,51 @@
-var intervalId 
-var counter=0
-var target =200
+
+var target = 200
+var intervalId
+
+function play(interval){
+	document.getElementById('button_correct').click()
+	intervalId = setInterval(job, interval)
+	console.log("start = "+intervalId)
+}
+
+function job(){
+    var timeline_progress  = parseFloat(document.getElementById("timeline_progress").style.right)
+    var scorevalue = document.getElementById('result_score_value').innerHTML
+    console.log("scorevalue = "+scorevalue+"| gg = "+timeline_progress)
+    if(timeline_progress == 100 || scorevalue >= target){
+    	console.log("stop = "+intervalId)
+    	clearInterval(intervalId)
+    }else if(timeline_progress>=0){
+	    answer()
+    }else {
+
+    }
+}
+
 function answer(){
 	if(document.getElementById('task_x')!=null){
 		var x=document.getElementById('task_x').innerHTML
-		var operator = document.getElementById('task_op').innerHTML
-		if(operator=='x'){
+		var operator = document.getElementById('task_op').innerText
+		if(operator=='×'){
 			operator='*'
-		}else if(operator =='+'){
+		} else if(operator =='+'){
 			operator='+'
-		}else if(operator == '/'){
+		} else if(operator == '/'){
 			operator ='/'
-		}else {
+		} else {
 			operator ='-'
-		}
+		}  
+
 		var y=document.getElementById('task_y').innerHTML
 		var result = document.getElementById('task_res').innerHTML
 
-		if( eval(x + operator + y )== eval(result)){
+		if(eval(x + operator + y ) == eval(result)){
+			console.log(x+operator+y +'='+result) 
 			document.getElementById('button_correct').click()
-	    }else{
+	    }else {
+	    	console.log(x+operator+y +'='+ eval(x + operator + y ) +'!='+result) 
 			document.getElementById('button_wrong').click()
-		}
-
-		if(counter <= target){
-			counter++
-		}else{
-			clearInterval(intervalId)
 		}
 	}
 }
-intervalId = setInterval(answer, 50);
+play(50)
